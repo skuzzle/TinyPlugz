2dc25c2a7b2b0dd7fb8460c6f710d589d09575b6
Simon Taddiken <simon.taddiken@gmail.com>
2015-04-24 11:05:17 +0200

    Add little package documentation

--- tiny-plugz-guice/src/main/java/de/skuzzle/tinyplugz/guice/package-info.java
+++ tiny-plugz-guice/src/main/java/de/skuzzle/tinyplugz/guice/package-info.java
@@ -1,4 +1,9 @@
 /**
+ * {@link de.skuzzle.tinyplugz.TinyPlugz TinyPlugz} service provider
+ * implementation supporting <em>dependency injection</em> using Google Guice.
+ * See {@link de.skuzzle.tinyplugz.guice.TinyPlugzGuice TinyPlugzGuice} for
+ * usage instructions.
+ *
  * @author Simon Taddiken
  */
 package de.skuzzle.tinyplugz.guice;
