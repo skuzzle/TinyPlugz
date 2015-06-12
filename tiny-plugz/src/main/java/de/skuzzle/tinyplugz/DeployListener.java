d36ad2151d738df86960b42ef516a97e88a9334f
Simon Taddiken <simon.taddiken@gmail.com>
2015-06-12 11:18:57 +0200

    Improve documentation

--- tiny-plugz/src/main/java/de/skuzzle/tinyplugz/DeployListener.java
+++ tiny-plugz/src/main/java/de/skuzzle/tinyplugz/DeployListener.java
@@ -3,8 +3,10 @@ package de.skuzzle.tinyplugz;
 import java.util.Map;
 
 /**
- * Listener which is notified when a TinyPlugz instance is deployed. Implementations of
- * this listener must be registered as a Java Service Provider.
+ * Listener which is notified when a TinyPlugz instance is deployed.
+ * Implementations of this listener must be registered as a Java Service
+ * Provider. They are pulled in after deployment using the plugin ClassLoader.
+ * This means that listener implementations can also reside within plugins.
  *
  * @author Simon Taddiken
  * @since 0.2.0
@@ -12,10 +14,13 @@ import java.util.Map;
 public interface DeployListener {
 
     /**
-     * Notified when TinyPlugz has been deployed.
+     * Notified when TinyPlugz has been deployed. If this method throws any
+     * unchecked exceptions, those will be logged and then ignored to continue
+     * notifying the next listener.
      *
      * @param tinyPlugz The deployed instance.
-     * @param properties The properties with which the instance has been configured.
+     * @param properties The properties with which the instance has been
+     *            configured.
      */
     void initialized(TinyPlugz tinyPlugz, Map<Object, Object> properties);
 }
